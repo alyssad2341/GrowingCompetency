1213b7e5911bb67b03964e34dc15d9baa5ae2265
alyss <alyssaduranovic@gmail.com>
2025-10-16 22:44:54 -0700

    Add String for name
    Added String with value Alys to print to console

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         int num = 4;
+        String name = "Alyssa";
         System.out.println("Hello World!" + num);
+        System.out.println(name);
     }
 }
